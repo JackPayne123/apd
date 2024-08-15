Fixed a tensor / gradient / copy bug in piecewise_models handcoded initialization

spd/models/piecewise_models.py
@@ -184,9 +184,9 @@ class ControlledPiecewiseLinear(nn.Module):
                 i * self.num_neurons : (i + 1) * self.num_neurons
             ] = -self.negative_suppression
 
-            self.input_layer.weight.data[
-                i * self.num_neurons : (i + 1) * self.num_neurons, 0
-            ] = piecewise_linear.input_layer.weight.data.squeeze()
+            self.input_layer.weight.data[i * self.num_neurons : (i + 1) * self.num_neurons, 0] = (
+                piecewise_linear.input_layer.weight.data.squeeze()
+            )
             self.input_layer.weight.data[i * self.num_neurons : (i + 1) * self.num_neurons, 1:] += (
                 self.control_W_E[i]
                 * (self.negative_suppression + piecewise_linear.input_layer.bias.data.unsqueeze(1))
@@ -198,9 +198,9 @@ class ControlledPiecewiseLinear(nn.Module):
         for i in range(self.num_functions):
             piecewise_linear = self.piecewise_linears[i]
             self.output_layer.bias.data[i] = piecewise_linear.output_layer.bias.data
-            self.output_layer.weight.data[
-                i, i * self.num_neurons : (i + 1) * self.num_neurons
-            ] = piecewise_linear.output_layer.weight.data.squeeze()
+            self.output_layer.weight.data[i, i * self.num_neurons : (i + 1) * self.num_neurons] = (
+                piecewise_linear.output_layer.weight.data.squeeze()
+            )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         control_bits = x[:, 1:]
@@ -350,11 +350,11 @@ class ControlledResNet(nn.Module):
         # set the weights of the residual layers to be the weights of the corresponding neurons in
         # the controlled piecewise linear
         for i in range(self.n_layers):
-            self.mlps[i].input_layer.weight.data[
-                :, :-1
-            ] = self.controlled_piecewise_linear.input_layer.weight.data[
-                self.neuron_permutations[i]
-            ]
+            self.mlps[i].input_layer.weight.data[:, :-1] = (
+                self.controlled_piecewise_linear.input_layer.weight.data[
+                    self.neuron_permutations[i]
+                ]
+            )
             self.mlps[
                 i
             ].input_layer.bias.data = self.controlled_piecewise_linear.input_layer.bias.data[
@@ -703,7 +703,7 @@ class PiecewiseFunctionSPDTransformer(SPDModel):
         # Input layer
         self.mlps[0].linear1.A.data[:k, :] = torch.eye(k)
         self.mlps[0].linear1.A.data[-1, :] = torch.zeros(k)
-        self.mlps[0].linear1.B.data = target_transformer.mlps[0].input_layer.weight.T[:k, :]
+        self.mlps[0].linear1.B.data[:, :] = target_transformer.mlps[0].input_layer.weight.T[:k, :]
         # Output layer
         original_Wout_last_col = target_transformer.mlps[0].output_layer.weight.T[:, -1]
         norm = torch.norm(original_Wout_last_col, dim=0)