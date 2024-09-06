b12084734beec84d2355f78ce4b859ea8ab315e6
Dan Braun <dan@apolloresearch.ai>
2024-09-06 14:49:33 +0000

    Plot attributions rather than activations

--- spd/experiments/linear/linear_decomposition.py
+++ spd/experiments/linear/linear_decomposition.py
@@ -23,9 +23,8 @@ from spd.log import logger
 from spd.run_spd import Config, DeepLinearConfig, optimize
 from spd.utils import (
     DatasetGeneratedDataLoader,
-    calc_attributions_full_rank,
-    calc_attributions_rank_one,
-    calc_topk_mask,
+    calc_attributions_full_rank_per_layer,
+    calc_attributions_rank_one_per_layer,
     init_wandb,
     load_config,
     permute_to_identity,
@@ -99,15 +98,15 @@ def _plot_multiple_subnetwork_params(
     return fig
 
 
-def _plot_subnetwork_activations_fn(
+def _plot_subnetwork_attributions_fn(
     batch: Float[Tensor, "batch n_instances n_features"],
-    inner_acts: list[Float[Tensor, "batch n_instances k"]],
+    attributions: list[Float[Tensor, "batch n_instances k"]],
 ) -> plt.Figure:
-    """Plot the inner acts for the first batch_elements in the batch.
+    """Plot the attributions for the first batch_elements in the batch.
 
-    The first row is the raw batch information, the following rows are the inner acts per layer.
+    The first row is the raw batch information, the following rows are the attributions per layer.
     """
-    n_layers = len(inner_acts)
+    n_layers = len(attributions)
     n_instances = batch.shape[1]
 
     fig, axs = plt.subplots(
@@ -138,15 +137,15 @@ def _plot_subnetwork_activations_fn(
         ax.set_xticks([])
         ax.set_yticks([])
 
-    # Add the inner acts
+    # Add the attributions
     for layer in range(n_layers):
         for i in range(n_instances):
             ax = axs[layer + 1, i]
-            instance_data = inner_acts[layer][:, i, :].abs().detach().cpu().float().numpy()
+            instance_data = attributions[layer][:, i, :].abs().detach().cpu().float().numpy()
             ax.matshow(instance_data, vmin=0, vmax=np.max(instance_data), cmap=cmap)
 
             if i == 0:
-                ax.set_ylabel(f"h_{layer}")
+                ax.set_ylabel(f"Layer {layer}")
             elif i == n_instances - 1:
                 ax.set_ylabel("batch_idx", rotation=-90, va="bottom", labelpad=15)
                 ax.yaxis.set_label_position("right")
@@ -161,16 +160,14 @@ def _plot_subnetwork_activations_fn(
     return fig
 
 
-def _collect_permuted_subnetwork_activations(
+def _collect_permuted_subnetwork_attributions(
     model: DeepLinearComponentModel | DeepLinearComponentFullRankModel,
     device: str,
-    topk: float | None = None,
-    batch_topk: bool = True,
 ) -> tuple[
     Float[Tensor, "batch n_instances n_features"], list[Float[Tensor, "batch n_instances k"]]
 ]:
     """
-    Collect subnetwork activations and permute them for visualization.
+    Collect subnetwork attributions and permute them for visualization.
 
     This function creates a test batch using an identity matrix, passes it through the model,
     and collects the attributions, and then permutes them to align with the identity.
@@ -179,12 +176,10 @@ def _collect_permuted_subnetwork_activations(
         model (DeepLinearComponentModel | DeepLinearComponentFullRankModel): The model to collect
             attributions on.
         device (str): The device to run computations on.
-        topk (int): The number of topk indices to use for the forward pass.
 
     Returns:
         - The input test batch (identity matrix expanded over instance dimension).
         - A list of permuted attributions for each layer.
-
     """
     test_batch = einops.repeat(
         torch.eye(model.n_features, device=device),
@@ -193,35 +188,26 @@ def _collect_permuted_subnetwork_activations(
     )
 
     out, test_layer_acts, test_inner_acts = model(test_batch)
-    if topk is not None:
-        if isinstance(model, DeepLinearComponentModel):
-            attribution_scores = calc_attributions_rank_one(out=out, inner_acts=test_inner_acts)
-        else:
-            assert isinstance(model, DeepLinearComponentFullRankModel)
-            attribution_scores = calc_attributions_full_rank(
-                out=out, inner_acts=test_inner_acts, layer_acts=test_layer_acts
-            )
-        topk_mask = calc_topk_mask(attribution_scores, topk, batch_topk=batch_topk)
+    if isinstance(model, DeepLinearComponentModel):
+        layer_attributions = calc_attributions_rank_one_per_layer(
+            out=out, inner_acts=test_inner_acts
+        )
+    else:
+        assert isinstance(model, DeepLinearComponentFullRankModel)
+        layer_attributions = calc_attributions_full_rank_per_layer(
+            out=out, inner_acts=test_inner_acts, layer_acts=test_layer_acts
+        )
 
-        test_inner_acts = model.forward_topk(test_batch, topk_mask=topk_mask)[-1]
-
-    # Full rank has an extra n_features dimension which we sum over
-    if isinstance(model, DeepLinearComponentFullRankModel):
-        for i in range(len(test_inner_acts)):
-            test_inner_acts[i] = einops.einsum(
-                test_inner_acts[i], "batch n_instances k n_features -> batch n_instances k"
-            )
-
-    test_inner_acts_permuted = []
+    test_attributions_permuted = []
     for layer in range(model.n_layers):
-        test_inner_acts_layer_permuted = []
+        test_attributions_layer_permuted = []
         for i in range(model.n_instances):
-            test_inner_acts_layer_permuted.append(
-                permute_to_identity(test_inner_acts[layer][:, i, :].abs())
+            test_attributions_layer_permuted.append(
+                permute_to_identity(layer_attributions[layer][:, i, :].abs())
             )
-        test_inner_acts_permuted.append(torch.stack(test_inner_acts_layer_permuted, dim=1))
+        test_attributions_permuted.append(torch.stack(test_attributions_layer_permuted, dim=1))
 
-    return test_batch, test_inner_acts_permuted
+    return test_batch, test_attributions_permuted
 
 
 def make_linear_plots(
@@ -229,17 +215,13 @@ def make_linear_plots(
     step: int,
     out_dir: Path | None,
     device: str,
-    topk: float | None,
-    batch_topk: bool,
     **_,
 ) -> dict[str, plt.Figure]:
-    test_batch, test_inner_acts = _collect_permuted_subnetwork_activations(
-        model, device, topk, batch_topk=batch_topk
-    )
+    test_batch, test_attributions = _collect_permuted_subnetwork_attributions(model, device)
 
-    act_fig = _plot_subnetwork_activations_fn(batch=test_batch, inner_acts=test_inner_acts)
+    act_fig = _plot_subnetwork_attributions_fn(batch=test_batch, attributions=test_attributions)
     if out_dir is not None:
-        act_fig.savefig(out_dir / f"inner_acts_{step}.png")
+        act_fig.savefig(out_dir / f"layer_attributions_{step}.png")
     plt.close(act_fig)
 
     param_fig = _plot_multiple_subnetwork_params(model, step)
@@ -248,9 +230,9 @@ def make_linear_plots(
     plt.close(param_fig)
 
     if out_dir is not None:
-        tqdm.write(f"Saved inner_acts to {out_dir / f'inner_acts_{step}.png'}")
+        tqdm.write(f"Saved layer_attributions to {out_dir / f'layer_attributions_{step}.png'}")
         tqdm.write(f"Saved subnetwork_params to {out_dir / f'subnetwork_params_{step}.png'}")
-    return {"inner_acts": act_fig, "subnetwork_params": param_fig}
+    return {"layer_attributions": act_fig, "subnetwork_params": param_fig}
 
 
 def main(
