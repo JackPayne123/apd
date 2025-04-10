d3a7c76cd22271155d82695aaf078571c2311543
Dan Braun <dan@apolloresearch.ai>
2025-04-10 04:43:31 +0000

    Add SSModel.from_pretrained()

--- spd/experiments/lm/models.py
+++ spd/experiments/lm/models.py
@@ -4,15 +4,26 @@ Defines a SSModel class that is a wrapper around a llama model from SimpleStorie
 
 import fnmatch
 from functools import partial
+from pathlib import Path
 from typing import Any
 
 import torch
 import torch.nn as nn
+import wandb
 from jaxtyping import Float
 from simple_stories_train.models.llama import Llama
+from simple_stories_train.models.model_configs import MODEL_CONFIGS
 from torch import Tensor
+from wandb.apis.public import Run
 
+from spd.configs import Config, LMTaskConfig
 from spd.models.components import Gate, GateMLP, LinearComponent
+from spd.types import WANDB_PATH_PREFIX, ModelPath
+from spd.wandb_utils import (
+    download_wandb_file,
+    fetch_latest_wandb_checkpoint,
+    fetch_wandb_run_dir,
+)
 
 
 class LinearComponentWithBias(nn.Module):
@@ -184,3 +195,34 @@ class SSModel(nn.Module):
             handle.remove()
 
         return out, cache
+
+    @classmethod
+    def from_pretrained(cls, path: ModelPath) -> tuple["SSModel", Config]:
+        if isinstance(path, str) and path.startswith(WANDB_PATH_PREFIX):
+            wandb_path = path.removeprefix(WANDB_PATH_PREFIX)
+            api = wandb.Api()
+            run: Run = api.run(wandb_path)
+            checkpoint_file_obj = fetch_latest_wandb_checkpoint(run, prefix="model")
+
+            run_dir = fetch_wandb_run_dir(run.id)
+            checkpoint_path = download_wandb_file(run, run_dir, checkpoint_file_obj.name)
+
+        else:
+            checkpoint_path = Path(path)  # local path
+
+        checkpoint_dict = torch.load(checkpoint_path, map_location="cpu")
+        config = Config(**checkpoint_dict["config"])
+
+        assert isinstance(config.task_config, LMTaskConfig)
+        model_config_dict = MODEL_CONFIGS[config.task_config.model_size]
+        model_path = f"chandan-sreedhara/SimpleStories-{config.task_config.model_size}"
+        llama_model = Llama.from_pretrained(model_path, model_config_dict)
+
+        ss_model = SSModel(
+            llama_model=llama_model,
+            target_module_patterns=config.task_config.target_module_patterns,
+            m=config.m,
+            n_gate_hidden_neurons=config.n_gate_hidden_neurons,
+        )
+        ss_model.load_state_dict(checkpoint_dict["model"])
+        return ss_model, config
